2f635af95deea882d7bb585701dbe890a5561266
MarcoPop32 <marcosv1881@gmail.com>
2026-08-18 14:42:54 -0400

    Exercicio 1

---
+++ Soma/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+ int n1, n2, soma;
+
+ printf("Insira o valor do 1° operador: ");
+ scanf("%d", &n1);
+ printf("Insira o valor do 2° operador: ");
+ scanf("%d", &n2);
+
+ soma = n1+n2;
+
+ printf("A soma dos dois números é igual a: %d", soma);
+
+ return 0;
+
+}
